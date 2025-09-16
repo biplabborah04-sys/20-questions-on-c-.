7d21d2dc2cef931f8adf9d0bba5e6c266546ead0
Biplab Borah <biplabborah04@gmail.com>
2025-09-16 09:16:49 +0530

    Add files via upload

---
+++ Q. 14 a C++ program to sort an array of n numbers in ascending and descending order using any sorting technique.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Function to sort array in ascending order
+void sortAscending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to sort array in descending order
+void sortDescending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to display the array
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sort in ascending order
+    sortAscending(arr, n);
+    cout << "\nArray in Ascending Order: ";
+    displayArray(arr, n);
+
+    // Sort in descending order
+    sortDescending(arr, n);
+    cout << "Array in Descending Order: ";
+    displayArray(arr, n);
+
+    return 0;
+}
+
