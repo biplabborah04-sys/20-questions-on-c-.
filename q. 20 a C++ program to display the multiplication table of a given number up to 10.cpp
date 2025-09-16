ce72fd224eab880df1e775b1ebb1793232173f37
Biplab Borah <biplabborah04@gmail.com>
2025-09-16 09:49:11 +0530

    Add files via upload

---
+++ q. 20 a C++ program to display the multiplication table of a given number up to 10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Input the number
+    cout << "Enter a number to display its multiplication table: ";
+    cin >> number;
+
+    // Display the multiplication table up to 10
+    cout << "\nMultiplication Table of " << number << ":\n";
+    for (int i = 1; i <= 10; i++) {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+
+    return 0;
+}
+
